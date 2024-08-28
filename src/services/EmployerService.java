42a1813af785d7c636383008fca1d74ce2dab5a5
Ziyad091 <z.bougdou@polyswantech.com>
2024-08-28 16:58:54 +0200

    new fonction

--- src/services/EmployerService.java
+++ src/services/EmployerService.java
@@ -1,5 +1,33 @@
 package services;
 
+import java.time.LocalDate;
+import java.util.Random;
+
+import classes.Employer;
+
 public class EmployerService {
 
+	public EmployerService() {
+		
+	}
+	
+	public Employer create(int id, String nom, String prenom, int age, String sexe, String pole, LocalDate dateEntree, LocalDate dateSortie, float salaire, boolean actif ) {
+		
+		return new Employer(id, nom, prenom, age, sexe, pole, dateEntree, dateSortie, salaire, actif);
+		
+		
+	}
+	 
+	
+	public String getInformation(Employer employer) {
+		
+		
+			
+	}
+		
+	
+		
+		
+	}
+	
 }
